a220b6e43bc1c5efc4e883ef7be1278f0f05f8f7
ANEES ALAM <anees.itlucknow@gmail.com>
2024-10-28 11:41:34 +0530

    Third changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Header from './Component/Header'
+import Input1 from './Component/Input1'
+export default function App() {
+  function process()
+  {
+    alert("hello");
+  }
+  var c=0;
+  
+  function inputhandles(event)
+  {
+       if(event.key=="Backspace")
+       {
+        c=c-1;
+       }
+       else
+       {
+        c=c+1;
+       }
+       document.getElementById("sp").innerHTML="Count "+c;
+  }
+  var count=0;
+  function inputHandle(event)
+  {
+    
+    if (event.key === "Backspace") 
+      {
+        if(count<0)
+          count=0;
+        else
+        count=count-1;
+      }
+      else
+      {
+        count=count+1;
+      }
+      document.getElementById("sp").innerHTML="Count "+count;
+      if(count>5)
+      {
+           document.getElementById("inputbox").disabled=true;
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container mt-4 text-primary">
+      <h3>Task Input Control</h3>
+      <hr></hr>
+      <Header></Header>
+      <Input1></Input1>
+      <br></br>
+      <button  onClick={process}>Click Here To Call The Function</button>
+      <hr />
+      <input onChange={inputhandles} type="text" placeholder='Type some thing' />
+      <hr>
+      </hr>
+      <input id="inputbox" onKeyDown={inputHandle} type="text" placeholder='reset function'></input>
+      <span id="sp">
 
-export default App;
+      </span>
+
+    </div>
+  )
+}
